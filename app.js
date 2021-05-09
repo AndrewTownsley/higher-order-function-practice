a5c77c8387f5ada8b3680f18f909b96837b14e2b
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-08 22:31:19 -0600

    map,filter,sort,reduce

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const companies= [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // for(let i = 0; i < companies.length; i++) {
-//   console.log(companies[i]);
 // }
 
 // .forEach()
@@ -29,9 +28,50 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // function canDrink() {
 //   ages.filter(age => age >= 21);
 // }
-const canDrink = ages.filter(age => age >= 21)
-// console.log(canDrink);
+// const canDrink = ages.filter(age => age >= 21)
 
 // Filter the retail companies
-const filterRetail = companies.filter(company => company.category.retail);
-console.log(filterRetail);
+const filterRetail = companies.filter(company => company.category === "Retail" );
+
+// const retailFilter = companies.filter(function(company) {
+//   if(company.category === "Retail") {
+//     return true;
+//   }
+// });
+
+
+// Filter out companies that started in the 1980s
+
+const filterEighties = companies.filter(company => company.start > 1979 && company.start < 1990);
+
+// Get all the companies that lasted at least 10 years
+const tenYears = companies.filter(company => company.end - company.start > 9);
+
+// MAP// MAP // MAP // MAP //
+
+// Create array of company names.
+
+// const companyNames = companies.map(company => company.name);
+// Return a string with the comapny name and how long the comapny lasted.
+const companyNames = companies.map(function(company) {
+  return `${company.name} [${company.end - company.start}]`
+});
+
+// Return a string with the comapny name and how long it lasted, sorted from longest to shortest.
+// Figure out how long each company lasted.
+const howLong = companies.map(company => company.end - company.start);
+// Sort the companies by how long they lasted
+const companySort = howLong.sort((a, b) => a > b ? -1 : 1)
+// Then map the company name onto the array of sorted companies.
+// const nameAndYears = companySort.map(company => 
+//   `${company.name } - ${(company.end - company.start)} years`)
+// console.log(howLong);
+
+// Square all of the ages
+const squareAges = ages.map(age => `${age} [${Math.round(Math.sqrt(age))}]`);
+
+// SORT// SORT // SORT // SORT //
+// Sort Companies by start year.
+const sortedCompanies = companies.sort((a, b) => 
+  a.start > b.start ? 1 : -1);
+  console.log(sortedCompanies);
